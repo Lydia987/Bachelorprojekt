deb4707721302bf94a25d81d175fb9eb842187f4
lydia987 <engelhardt.lydia.post@gmail.com>
2022-05-01 22:29:18 +0200

    added decimal import zu stop

--- follow_me/src/follow_pkg/scripts/Stop.py
+++ follow_me/src/follow_pkg/scripts/Stop.py
@@ -3,11 +3,11 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import NavSatFix, Imu
+from sensor_msgs.msg import NavSatFix
 from geometry_msgs.msg import Twist
-from tf.transformations import euler_from_quaternion
 from std_msgs.msg import String
 import numpy as np
+import decimal as d
 
 
 class Stop:
@@ -25,17 +25,17 @@ class Stop:
         self.sub_roboter_gps = rospy.Subscriber('/navsat/fix', NavSatFix, self.set_pos)
         self.sub_vel = rospy.Subscriber('/cmd_vel', Twist, self.set_velocity)
 
-        self.counter = 2500
+        self.counter = 20
         self.msg = "drive"
         self.vel = 0
         self.rate = rospy.Rate(10)
-        self.actual_pos = [0.0, 0.0]  # [latitude,longitude]
-        self.old_pos = [0.0, 0.0]  # [latitude,longitude]
+        self.actual_pos = [d.Decimal(0), d.Decimal(0)]  # [latitude,longitude]
+        self.old_pos = [d.Decimal(0), d.Decimal(0)]  # [latitude,longitude]
 
     # Stops the robot if something is nearer than 0,4 meter
     def laser_callback(self, scan_msg):
         for i in scan_msg.ranges:
-            if (i <= 0.4 or (i <= 0.5 and self.msg == "stop")) and not np.isinf(i) and not np.isnan(i):
+            if (i <= 0.3 or (i <= 0.4 and self.msg == "stop")) and not np.isinf(i) and not np.isnan(i):
                 self.msg = "stop"
                 break
             else:
@@ -45,6 +45,7 @@ class Stop:
     def set_pos(self, data):
         self.actual_pos[0] = data.latitude
         self.actual_pos[1] = data.longitude
+        print("pos", data.latitude, data.longitude)
 
     # the velocity of the robot
     def set_velocity(self, data):
@@ -52,13 +53,14 @@ class Stop:
 
     def run(self):
         while not rospy.is_shutdown():
-            dist = abs(self.old_pos[0] - self.actual_pos[0]) + (abs(self.old_pos[1] - self.actual_pos[1]))
-            if dist == 0 and not(self.vel == 0):
+            dist = d.Decimal(d.Decimal(np.sqrt(d.Decimal(d.Decimal(d.Decimal(self.old_pos[0] - self.actual_pos[0]) * d.Decimal(111.3)) ** 2) + d.Decimal(d.Decimal(d.Decimal(self.old_pos[1] - self.actual_pos[1]) * d.Decimal(71.5)) ** 2))))
+            print(dist)
+            if dist < 0.0001 and not(self.vel == 0) and self.counter == 0:
                 self.msg = "festgefahren"
 
             if self.counter == 0:
                 self.old_pos = self.actual_pos
-                self.counter = 2500
+                self.counter = 20
             else:
                 self.counter -= 1
 
